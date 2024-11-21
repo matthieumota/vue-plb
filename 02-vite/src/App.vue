45ba72efed27ed6cb5cc473b6ca7a75418815709
Matthieu Mota <matthieumota@gmail.com>
2024-11-21 12:13:02 +0100

    fix

--- 02-vite/src/App.vue
+++ 02-vite/src/App.vue
@@ -11,17 +11,17 @@ const contacts = ref(['Marina', 'Matthieu'])
 const event = 'click'
 
 const toUpper = (c) => {
-  console.log('computedUPPER '+Math.random())
+  console.log('computedUPPER ' + Math.random())
   return c.toUpperCase()
 }
 
 const contactsWithUpper = computed(() => {
-  console.log('computedUPPER '+Math.random())
-  return contacts.value.map(c => c.toUpperCase())
+  console.log('computedUPPER ' + Math.random())
+  return contacts.value.map((c) => c.toUpperCase())
 })
 
 const reversedTitle = computed(() => {
-  console.log('computed '+Math.random())
+  console.log('computed ' + Math.random())
   return title.value.split('').reverse().join('')
 })
 
@@ -37,37 +37,59 @@ const products = ref([
   {
     brand: 'Brand',
     name: 'Cat',
-    image: 'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
+    image:
+      'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
     price: 1297.12,
     stock: 100,
     features: ['Mignon', 'Affectueux', 'Gourmand'],
     variations: [
-      { color: 'blue', price: 0, image: 'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg' },
-      { color: 'red', price: 2, image: 'https://cdn.shopify.com/s/files/1/0265/1327/7008/files/comment-accueillir-un-chaton.jpg' },
+      {
+        color: 'blue',
+        price: 0,
+        image:
+          'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
+      },
+      {
+        color: 'red',
+        price: 2,
+        image:
+          'https://cdn.shopify.com/s/files/1/0265/1327/7008/files/comment-accueillir-un-chaton.jpg',
+      },
       { color: 'green', price: 6 },
       { color: 'purple', price: 4 },
-    ]
+    ],
   },
   {
     brand: 'Brand 2',
     name: 'Dog',
-    image: 'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
+    image:
+      'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
     price: 657.12,
     stock: 100,
     features: ['A', 'B', 'C'],
     variations: [
-      { color: 'blue', price: 0, image: 'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg' },
-      { color: 'red', price: 2, image: 'https://cdn.shopify.com/s/files/1/0265/1327/7008/files/comment-accueillir-un-chaton.jpg' },
+      {
+        color: 'blue',
+        price: 0,
+        image:
+          'https://www.zooplus.fr/magazine/wp-content/uploads/2022/03/kitten-sitzt-boden-768x512-1.jpeg',
+      },
+      {
+        color: 'red',
+        price: 2,
+        image:
+          'https://cdn.shopify.com/s/files/1/0265/1327/7008/files/comment-accueillir-un-chaton.jpg',
+      },
       { color: 'green', price: 6 },
       { color: 'purple', price: 4 },
-    ]
-  }
+    ],
+  },
 ])
 const cart = ref([]) // { quantity: 3, fullName: 'Brand Cat', total: 10 }
 
 const add = ({ quantity, fullName, total }) => {
   // si l'item est déjà dans le tableau
-  const item = cart.value.find(i => i.fullName === fullName)
+  const item = cart.value.find((i) => i.fullName === fullName)
 
   if (item) {
     item.quantity += quantity
@@ -90,7 +112,7 @@ const add = ({ quantity, fullName, total }) => {
 
     <h2>{{ reversedTitle }}</h2>
     <h2>{{ reversedTitle }}</h2>
-    <input type="text" v-model="name">
+    <input type="text" v-model="name" />
     <h2>{{ name }}</h2>
 
     <ul>
